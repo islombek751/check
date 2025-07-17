6415ea5e71007e3b3a685bc4b7dfa03e4496b57a
islombek <tanlanganasarlar@gmail.com>
2025-07-17 16:55:34 +0500

    ccc

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ source venv/bin/activate
 export $(cat .env | xargs)
 
 exec gunicorn src.main:app \
-    --workers 4 \
+    --workers 2 \
     --worker-class uvicorn.workers.UvicornWorker \
     --bind 127.0.0.1:8000 \
     --timeout 120
